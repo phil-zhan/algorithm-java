eb1a16d71a7f7da7b786e3518fa8ded4b190c510
詹发文 <zhanfw@bsoft.com.cn>
2021-10-04 16:50:31 +0800

    第三十一次课程class31

--- src/org/study/class30/MainTest01.java
+++ src/org/study/class30/MainTest01.java
@@ -5,5 +5,191 @@ package org.study.class30;
  * @date 2021/10/3 15:44
  */
 public class MainTest01 {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
 
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static void process(Node head) {
+        if (null == head) {
+            return;
+        }
+        // System.out.println(head.value);
+        process(head.left);
+        // System.out.println(head.value);
+        process(head.right);
+        System.out.println(head.value);
+    }
+
+
+    public static void morris(Node head) {
+        if (null == head) {
+            return;
+        }
+
+        Node cur = head;
+        Node mostRight;
+        while (null != cur) {
+            mostRight = cur.left;
+            if (null != mostRight) {
+                while (null != mostRight.right && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (null == mostRight.right) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+
+            cur = cur.right;
+
+        }
+    }
+
+    public static void morrisPre(Node head) {
+        if (null == head) {
+            return;
+        }
+
+        Node cur = head;
+        Node mostRight;
+        while (null != cur) {
+            mostRight = cur.left;
+            if (null != mostRight) {
+                while (null != mostRight.right && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+
+                if (null == mostRight.right) {
+
+                    System.out.println(cur.value);
+
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }else{
+                System.out.println(cur.value);
+            }
+
+            cur = cur.right;
+
+        }
+    }
+
+    public static void morrisIn(Node head) {
+        if (null == head) {
+            return;
+        }
+
+        Node cur = head;
+        Node mostRight;
+        while (null != cur) {
+            mostRight = cur.left;
+            if (null != mostRight) {
+                while (null != mostRight.right && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (null == mostRight.right) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+
+                    mostRight.right = null;
+                }
+            }
+
+            // 左边遍历完成或者是没有左
+            System.out.println(cur.value);
+
+            cur = cur.right;
+
+        }
+    }
+
+    /**
+     * morris 改为后序
+     * @date 2021-10-03 13:39:16
+     */
+    public static void morrisPos(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+
+                    // 第二次到大
+                    // 逆序打印其左树的右边界
+                    printEdge(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+
+        // 遍历完成，打印整棵树的右边界
+        printEdge(head);
+        System.out.println();
+    }
+
+
+    public static void printEdge(Node head){
+        Node tail = reverseEdge(head);
+        Node cur = tail;
+        while (cur != null){
+            System.out.println(cur.value);
+            cur = cur.right;
+        }
+        reverseEdge(tail);
+    }
+
+    public static Node reverseEdge(Node from) {
+        Node pre = null;
+        Node next;
+        while (from != null) {
+            next = from.right;
+            from.right = pre;
+            pre = from;
+            from = next;
+        }
+        return pre;
+    }
+
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.left = new Node(2);
+        head.right = new Node(6);
+        head.left.left = new Node(1);
+        head.left.right = new Node(3);
+        head.right.left = new Node(5);
+        head.right.right = new Node(7);
+        process(head);
+
+        // morrisPre(head);
+        // morrisIn(head);
+        morrisPos(head);
+    }
 }
---
+++ src/org/study/class30/MainTest02.java
@@ -0,0 +1,136 @@
+package org.study.class30;
+
+/**
+ * @author phil
+ * @date 2021/10/4 10:05
+ */
+public class MainTest02 {
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node(int x) {
+            val = x;
+        }
+    }
+
+    public static int minHeight1(Node head) {
+        if (null == head) {
+            return 0;
+        }
+
+        return process(head);
+    }
+
+    public static int process(Node node) {
+        if (node.left == null && node.right == null) {
+            return 1;
+        }
+
+        int leftHeight = Integer.MAX_VALUE;
+        if (null != node.left) {
+            leftHeight = process(node.left);
+        }
+
+        int rightHeight = Integer.MAX_VALUE;
+        if (null != node.right) {
+            rightHeight = process(node.right);
+        }
+
+        return 1+Math.min(leftHeight, rightHeight);
+    }
+
+
+    public static int minHeight2(Node head) {
+        if (null == head) {
+            return 0;
+        }
+
+        Node cur = head;
+        Node mostRight = null;
+        int curLevel = 0;
+        int mostMinHeight = Integer.MAX_VALUE;
+
+        while (null != cur) {
+            mostRight = cur.left;
+
+            if (mostRight != null) {
+                int rightBoardSize = 1;
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                    rightBoardSize++;
+                }
+                if (mostRight.right == null) {
+
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    curLevel ++;
+                    continue;
+                } else {
+                    // 刚刚从 mostRight 跳过来。在此计算 mostRight 的最低高度
+                    if(mostRight.left == null){
+                        mostMinHeight = Math.min(curLevel,mostMinHeight);
+                    }
+
+                    curLevel -= rightBoardSize;
+
+                    mostRight.right = null;
+                    // 至此，左树的遍历已经完成了
+                }
+            }else{
+                curLevel++;
+            }
+
+            cur = cur.right;
+        }
+
+
+        // 遍历结束后，判断整棵树的最右节点的深度与之前的深度谁最小
+        int finalRight = 1;
+        cur = head;
+        while (cur.right != null) {
+            finalRight++;
+            cur = cur.right;
+        }
+        if (cur.left == null && cur.right == null) {
+            mostMinHeight = Math.min(mostMinHeight, finalRight);
+        }
+
+        return mostMinHeight;
+
+    }
+
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+    public static void main(String[] args) {
+        int treeLevel = 10;
+        int nodeMaxValue = 5;
+        int testTimes = 10000000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(treeLevel, nodeMaxValue);
+            int ans1 = minHeight1(head);
+            int ans2 = minHeight2(head);
+            if (ans1 != ans2) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test finish!");
+
+    }
+}
