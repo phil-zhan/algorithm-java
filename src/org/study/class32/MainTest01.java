5a906cb72e9c93c73f0c7194817ecb46509949b5
詹发文 <zhanfw@bsoft.com.cn>
2021-10-07 16:06:34 +0800

    第三十三次课程class33

--- src/org/study/class32/MainTest01.java
+++ src/org/study/class32/MainTest01.java
@@ -6,4 +6,45 @@ package org.study.class32;
  */
 public class MainTest01 {
 
+    public static class IndexTree{
+
+        private final int[] tree;
+
+        private final int length;
+
+        public IndexTree(int size){
+            this.length = size + 1;
+            this.tree = new int[length];
+        }
+
+
+        public void insert(int index , int value){
+            while (index <= length){
+                tree[index] += value;
+                index += index & -index;
+            }
+        }
+
+        public int sum(int index){
+            int answer = 0;
+
+            while (index > 0){
+                answer += tree[index];
+                index -= index & (~index+1);
+            }
+
+            return answer;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        IndexTree indexTree = new IndexTree(10);
+        for (int i = 1; i <= 10; i++) {
+            indexTree.insert(i,i);
+        }
+
+        System.out.println(indexTree.sum(3)-indexTree.sum(2));
+    }
 }
