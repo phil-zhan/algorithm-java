9507652b817a89ee77dcfb4864027daad9acbea9
zfw <zfw3549@163.com>
2022-02-14 00:20:32 +0800

    modify code

--- src/org/study/class21/Code01_MinPathSum.java
+++ src/org/study/class21/Code01_MinPathSum.java
@@ -2,47 +2,47 @@ package org.study.class21;
 
 public class Code01_MinPathSum {
 
-	public static int minPathSum1(int[][] m) {
-		if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+	public static int minPathSum1(int[][] grid) {
+		if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
 			return 0;
 		}
-		int row = m.length;
-		int col = m[0].length;
+		int row = grid.length;
+		int col = grid[0].length;
 		int[][] dp = new int[row][col];
-		dp[0][0] = m[0][0];
+		dp[0][0] = grid[0][0];
 		for (int i = 1; i < row; i++) {
-			dp[i][0] = dp[i - 1][0] + m[i][0];
+			dp[i][0] = dp[i - 1][0] + grid[i][0];
 		}
 		for (int j = 1; j < col; j++) {
-			dp[0][j] = dp[0][j - 1] + m[0][j];
+			dp[0][j] = dp[0][j - 1] + grid[0][j];
 		}
 		for (int i = 1; i < row; i++) {
 			for (int j = 1; j < col; j++) {
-				dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + m[i][j];
+				dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
 			}
 		}
 		return dp[row - 1][col - 1];
 	}
 
-	public static int minPathSum2(int[][] m) {
-		if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+	public static int minPathSum2(int[][] grid) {
+		if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
 			return 0;
 		}
-		int row = m.length;
-		int col = m[0].length;
+		int row = grid.length;
+		int col = grid[0].length;
 		int[] dp = new int[col];
-		dp[0] = m[0][0];
+		dp[0] = grid[0][0];
 
 		// 初始化第 0 行
 		for (int j = 1; j < col; j++) {
-			dp[j] = dp[j - 1] + m[0][j];
+			dp[j] = dp[j - 1] + grid[0][j];
 		}
 
 		// 向下滚动
 		for (int i = 1; i < row; i++) {
-			dp[0] += m[i][0];
+			dp[0] += grid[i][0];
 			for (int j = 1; j < col; j++) {
-				dp[j] = Math.min(dp[j - 1], dp[j]) + m[i][j];
+				dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j];
 			}
 		}
 		return dp[col - 1];
