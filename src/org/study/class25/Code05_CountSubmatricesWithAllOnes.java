54ba6b6647e63f0d39ea096d12fa7e4c806f961b
zfw <zfw3549@163.com>
2022-02-16 21:06:33 +0800

    modify code

--- src/org/study/class25/Code05_CountSubmatricesWithAllOnes.java
+++ src/org/study/class25/Code05_CountSubmatricesWithAllOnes.java
@@ -1,6 +1,11 @@
 package org.study.class25;
 
-// 测试链接：https://leetcode.com/problems/count-submatrices-with-all-ones
+/**
+ * 测试链接：https://leetcode.com/problems/count-submatrices-with-all-ones
+ * 讨论以每行为底的子矩阵（内部全是1）
+ * 最后统计个数
+ *
+ */
 public class Code05_CountSubmatricesWithAllOnes {
 
 	public static int numSubmat(int[][] mat) {
@@ -24,21 +29,37 @@ public class Code05_CountSubmatricesWithAllOnes {
 			return 0;
 		}
 		int nums = 0;
+
+		//数组模拟栈结构
 		int[] stack = new int[height.length];
 		int si = -1;
 		for (int i = 0; i < height.length; i++) {
 			while (si != -1 && height[stack[si]] >= height[i]) {
 				int cur = stack[si--];
+
+				// 相等的情况下不要计算（直接把之前的出栈就好），等到最后一个出现的时候，一起算
+				// 基于单调栈的原理。两个相同的数相遇时，他们中间的数肯定都是大于他们的，
+				// 如果是小于的话，与到小于数的时候，就会将前面一个数弹出去
 				if (height[cur] > height[i]) {
+
 					int left = si == -1 ? -1 : stack[si];
+
+					// 以当前高度为高的矩形的宽度
 					int n = i - left - 1;
+
+					// 左边到不了的高度和右边到不了的高度  取一个最大值
 					int down = Math.max(left == -1 ? 0 : height[left], height[i]);
+
+					// 计算数量。是一个等差数列。注意差值之间的高度（当前高度和左右两边到不了的位置的高度）
 					nums += (height[cur] - down) * num(n);
 				}
 
 			}
+			// 当前位置压栈
 			stack[++si] = i;
 		}
+
+		// 右边没数了，清理剩余的栈
 		while (si != -1) {
 			int cur = stack[si--];
 			int left = si == -1  ? -1 : stack[si];
--- src/org/study/class26/Code02_FibonacciProblem.java
+++ src/org/study/class26/Code02_FibonacciProblem.java
@@ -134,6 +134,18 @@ public class Code02_FibonacciProblem {
 		return 2 * res[0][0] + res[1][0];
 	}
 
+	/**
+	 *
+	 * 第一年农场有1只成熟的母牛A，往后的每年：
+	 *
+	 * 1）每一只成熟的母牛都会生一只母牛
+	 *
+	 * 2）每一只新出生的母牛都在出生的第三年成熟
+	 *
+	 * 3）每一只母牛永远不会死
+	 *
+	 * 返回N年后牛的数量
+	 */
 	public static int c1(int n) {
 		if (n < 1) {
 			return 0;
