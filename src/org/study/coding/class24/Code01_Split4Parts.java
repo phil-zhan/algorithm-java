8ee832213df75a4ef99bbd6885e6541fe1784289
zfw <zfw3549@163.com>
2022-04-05 22:09:52 +0800

    modify code

--- src/org/study/coding/class24/Code01_Split4Parts.java
+++ src/org/study/coding/class24/Code01_Split4Parts.java
@@ -3,87 +3,127 @@ package org.study.coding.class24;
 import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * 1. 给定一个正数数组arr，长度一定大于6（>=7），一定要选3个数字做分割点，从而分出4个部分，并且每部分都有数
+ * 分割点的数字直接删除，不属于任何4个部分中的任何一个。返回有没有可能分出的4个部分累加和一样大
+ * 如：{3,2,3,7,4,4,3,1,1,6,7,1,5,2}。可以分成{3,2,3}、{4,4}、{1,1,6}、{1,5,2}。分割点是不算的！
+ * <p>
+ * 解法：
+ * 做一个前缀和的辅助数组
+ * 来到 i 位置。让 i 位置作为第一刀。有没有可能切出四个累加和一样的部分，
+ * 来到i位置。如果 i-1 位置的前缀和是 100 。arr[i] = 9 。
+ * 那么要使得第二个部分和第一个部分相等，就从i往后推，必须要找到一个前缀和等于209的位置。假设为j，那么j+1位置就是第二刀的位置
+ * 因为都是正数数组。越往后，前缀和越大。。找到了第一个209，后面就不会再有209了
+ * <p>
+ * 假设arr[j+1] = 6
+ * 从j+1位置往后推，必须要找到一个前缀和等于315的位置[100+9+100+6+100]。假设为x，那么x+1位置就是第三刀的位置
+ * 切完第三刀之后，看看剩余的累加和是不是100。
+ * <p>
+ * 在这个过程中，只要第一个209或第二个315，亦或者是第三刀之后，剩下的累加和不等于100.那么当前的第一刀的位置都是无效的
+ * <p>
+ * 为了方便找到某个前缀和是否存在。准备一个hash表
+ * key：某个前缀和
+ * value：该前缀和对应的出现的位置
+ *
+ * @since 2022-04-05 08:03:42
+ */
 public class Code01_Split4Parts {
 
-	public static boolean canSplits1(int[] arr) {
-		if (arr == null || arr.length < 7) {
-			return false;
-		}
-		HashSet<String> set = new HashSet<String>();
-		int sum = 0;
-		for (int i = 0; i < arr.length; i++) {
-			sum += arr[i];
-		}
-		int leftSum = arr[0];
-		for (int i = 1; i < arr.length - 1; i++) {
-			set.add(String.valueOf(leftSum) + "_" + String.valueOf(sum - leftSum - arr[i]));
-			leftSum += arr[i];
-		}
-		int l = 1;
-		int lsum = arr[0];
-		int r = arr.length - 2;
-		int rsum = arr[arr.length - 1];
-		while (l < r - 3) {
-			if (lsum == rsum) {
-				String lkey = String.valueOf(lsum * 2 + arr[l]);
-				String rkey = String.valueOf(rsum * 2 + arr[r]);
-				if (set.contains(lkey + "_" + rkey)) {
-					return true;
-				}
-				lsum += arr[l++];
-			} else if (lsum < rsum) {
-				lsum += arr[l++];
-			} else {
-				rsum += arr[r--];
-			}
-		}
-		return false;
-	}
+    public static boolean canSplits1(int[] arr) {
+        if (arr == null || arr.length < 7) {
+            return false;
+        }
+        HashSet<String> set = new HashSet<>();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        int leftSum = arr[0];
+        for (int i = 1; i < arr.length - 1; i++) {
+            set.add(leftSum + "_" + (sum - leftSum - arr[i]));
+            leftSum += arr[i];
+        }
+        int l = 1;
+        int lsum = arr[0];
+        int r = arr.length - 2;
+        int rsum = arr[arr.length - 1];
+        while (l < r - 3) {
+            if (lsum == rsum) {
+                String lkey = String.valueOf(lsum * 2 + arr[l]);
+                String rkey = String.valueOf(rsum * 2 + arr[r]);
+                if (set.contains(lkey + "_" + rkey)) {
+                    return true;
+                }
+                lsum += arr[l++];
+            } else if (lsum < rsum) {
+                lsum += arr[l++];
+            } else {
+                rsum += arr[r--];
+            }
+        }
+        return false;
+    }
 
-	public static boolean canSplits2(int[] arr) {
-		if (arr == null || arr.length < 7) {
-			return false;
-		}
-		// key 某一个累加和， value出现的位置
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int sum = arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			map.put(sum, i);
-			sum += arr[i];
-		}
-		int lsum = arr[0]; // 第一刀左侧的累加和
-		for (int s1 = 1; s1 < arr.length - 5; s1++) { // s1是第一刀的位置
-			int checkSum = lsum * 2 + arr[s1]; // 100 x 100   100*2 + x
-			if (map.containsKey(checkSum)) {
-				int s2 = map.get(checkSum); // j -> y
-				checkSum += lsum + arr[s2];
-				if (map.containsKey(checkSum)) { // 100 * 3 + x + y
-					int s3 = map.get(checkSum); // k -> z
-					if (checkSum + arr[s3] + lsum == sum) {
-						return true;
-					}
-				}
-			}
-			lsum += arr[s1];
-		}
-		return false;
-	}
+    /**
+     * 最优解
+     * @since 2022-04-05 20:58:28
+     */
+    public static boolean canSplits2(int[] arr) {
+        if (arr == null || arr.length < 7) {
+            return false;
+        }
+        // key 某一个累加和， value出现的位置
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int sum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            map.put(sum, i);
+            sum += arr[i];
+        }
 
-	public static int[] generateRondomArray() {
-		int[] res = new int[(int) (Math.random() * 10) + 7];
-		for (int i = 0; i < res.length; i++) {
-			res[i] = (int) (Math.random() * 10) + 1;
-		}
-		return res;
-	}
+        // 第一刀左侧的累加和
+        int lsum = arr[0];
 
-	public static void main(String[] args) {
-		int testTime = 3000000;
-		for (int i = 0; i < testTime; i++) {
-			int[] arr = generateRondomArray();
-			if (canSplits1(arr) ^ canSplits2(arr)) {
-				System.out.println("Error");
-			}
-		}
-	}
+        // s1是第一刀的位置
+        for (int s1 = 1; s1 < arr.length - 5; s1++) {
+
+            // 100 x 100   100*2 + x
+            int checkSum = lsum * 2 + arr[s1];
+            if (map.containsKey(checkSum)) {
+
+                // j -> y
+                int s2 = map.get(checkSum);
+                checkSum += lsum + arr[s2];
+
+                // 100 * 3 + x + y
+                if (map.containsKey(checkSum)) {
+
+                    // k -> z
+                    int s3 = map.get(checkSum);
+                    if (checkSum + arr[s3] + lsum == sum) {
+                        return true;
+                    }
+                }
+            }
+            lsum += arr[s1];
+        }
+        return false;
+    }
+
+    public static int[] generateRondomArray() {
+        int[] res = new int[(int) (Math.random() * 10) + 7];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = (int) (Math.random() * 10) + 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 3000000;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRondomArray();
+            if (canSplits1(arr) ^ canSplits2(arr)) {
+                System.out.println("Error");
+            }
+        }
+    }
 }
--- src/org/study/coding/class24/Code06_RemoveDuplicateLettersLessLexi.java
+++ src/org/study/coding/class24/Code06_RemoveDuplicateLettersLessLexi.java
@@ -1,74 +1,116 @@
 package org.study.coding.class24;
 
-// 本题测试链接 : https://leetcode.com/problems/remove-duplicate-letters/
+/**
+ * 6. 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）
+ * Leetcode题目：https://leetcode.com/problems/remove-duplicate-letters/
+ * Input: s = "bcabc"
+ * Output: "abc"
+ * <p>
+ * Input: s = "cbacdcbc"
+ * Output: "acdb"
+ * <p>
+ * 解法：
+ * 整体的思路是 [0...i]范围上选一个字符。选中之后，去[i+1...n-1] 选剩下的字符
+ * <p>
+ * 建立一个统计表map
+ * key：是输入字符串的字符
+ * value：是对应字符出现的次数
+ * <p>
+ * 从左往右遍历字符串。每来到一个字符，就将其对应的次数减减【表示从当前位置到最后，还有多少个该字符】
+ * 如果某个字符的次数减到0的时候,假设对应的位置是i，后面再没有这个字符了.
+ * 【这个字符必须在i位置之前选出来】【但是选的时候，尽可能在候选的这个范围上，先选ASCII最小的】
+ * 表示现在必须选该字符了，【0...i】范围上选一个字符，【这个字符尽可能选能选的字符中，ASCII最小的】。假设选的位置是k。 0<= k <= i
+ * 选完之后【假设选的是 'a' 】,将[k+1...n-1]范围上的所有 'a' 全部删掉
+ * 删掉之后，再从[k+1...最后]范围上去考虑剩余的字符。
+ * 再从剩下的字符里面去选剩下的
+ * <p>
+ * 在选的时候，如果有多个 ASCII 最小的时候。选哪个都行
+ *
+ * @since 2022-04-05 21:40:14
+ */
 public class Code06_RemoveDuplicateLettersLessLexi {
 
-	// 在str中，每种字符都要保留一个，让最后的结果，字典序最小 ，并返回
-	public static String removeDuplicateLetters1(String str) {
-		if (str == null || str.length() < 2) {
-			return str;
-		}
-		int[] map = new int[256];
-		for (int i = 0; i < str.length(); i++) {
-			map[str.charAt(i)]++;
-		}
-		int minACSIndex = 0;
-		for (int i = 0; i < str.length(); i++) {
-			minACSIndex = str.charAt(minACSIndex) > str.charAt(i) ? i : minACSIndex;
-			if (--map[str.charAt(i)] == 0) {
-				break;
-			}
-		}
-		// 0...break(之前) minACSIndex
-		// str[minACSIndex] 剩下的字符串str[minACSIndex+1...] -> 去掉str[minACSIndex]字符 -> s'
-		// s'...
-		return String.valueOf(str.charAt(minACSIndex)) + removeDuplicateLetters1(
-				str.substring(minACSIndex + 1).replaceAll(String.valueOf(str.charAt(minACSIndex)), ""));
-	}
+    /**
+     * 最优解
+     * 在str中，每种字符都要保留一个，让最后的结果，字典序最小 ，并返回
+     * 时间复杂度 O(K*N) 【K是字符的种数。这个种数肯定不多】【也就可以看成O(N)的复杂度】
+     *
+     * @since 2022-04-05 21:40:25
+     */
+    public static String removeDuplicateLetters1(String str) {
+        if (str == null || str.length() < 2) {
+            return str;
+        }
+        int[] map = new int[256];
+        for (int i = 0; i < str.length(); i++) {
+            map[str.charAt(i)]++;
+        }
+        int minACSIndex = 0;
+        for (int i = 0; i < str.length(); i++) {
+            // 记录最小的 ASCII 的位置
+            minACSIndex = str.charAt(minACSIndex) > str.charAt(i) ? i : minACSIndex;
+            if (--map[str.charAt(i)] == 0) {
+                break;
+            }
+        }
+        // 0...break(之前) minACSIndex 【包含break的那个位置】
+        // str[minACSIndex] 剩下的字符串str[minACSIndex+1...] -> 去掉str[minACSIndex]字符 -> s'
+        // s'...
+        return str.charAt(minACSIndex) + removeDuplicateLetters1(
+                str.substring(minACSIndex + 1).replaceAll(String.valueOf(str.charAt(minACSIndex)), ""));
+    }
 
-	public static String removeDuplicateLetters2(String s) {
-		char[] str = s.toCharArray();
-		// 小写字母ascii码值范围[97~122]，所以用长度为26的数组做次数统计
-		// 如果map[i] > -1，则代表ascii码值为i的字符的出现次数
-		// 如果map[i] == -1，则代表ascii码值为i的字符不再考虑
-		int[] map = new int[26];
-		for (int i = 0; i < str.length; i++) {
-			map[str[i] - 'a']++;
-		}
-		char[] res = new char[26];
-		int index = 0;
-		int L = 0;
-		int R = 0;
-		while (R != str.length) {
-			// 如果当前字符是不再考虑的，直接跳过
-			// 如果当前字符的出现次数减1之后，后面还能出现，直接跳过
-			if (map[str[R] - 'a'] == -1 || --map[str[R] - 'a'] > 0) {
-				R++;
-			} else { // 当前字符需要考虑并且之后不会再出现了
-				// 在str[L..R]上所有需要考虑的字符中，找到ascii码最小字符的位置
-				int pick = -1;
-				for (int i = L; i <= R; i++) {
-					if (map[str[i] - 'a'] != -1 && (pick == -1 || str[i] < str[pick])) {
-						pick = i;
-					}
-				}
-				// 把ascii码最小的字符放到挑选结果中
-				res[index++] = str[pick];
-				// 在上一个的for循环中，str[L..R]范围上每种字符的出现次数都减少了
-				// 需要把str[pick + 1..R]上每种字符的出现次数加回来
-				for (int i = pick + 1; i <= R; i++) {
-					if (map[str[i] - 'a'] != -1) { // 只增加以后需要考虑字符的次数
-						map[str[i] - 'a']++;
-					}
-				}
-				// 选出的ascii码最小的字符，以后不再考虑了
-				map[str[pick] - 'a'] = -1;
-				// 继续在str[pick + 1......]上重复这个过程
-				L = pick + 1;
-				R = L;
-			}
-		}
-		return String.valueOf(res, 0, index);
-	}
+    /**
+     * 非递归的版本。
+     * 思路是一样的
+     *
+     * @since 2022-04-05 22:07:27
+     */
+    public static String removeDuplicateLetters2(String s) {
+        char[] str = s.toCharArray();
+        // 小写字母ascii码值范围[97~122]，所以用长度为26的数组做次数统计
+        // 如果map[i] > -1，则代表ascii码值为i的字符的出现次数
+        // 如果map[i] == -1，则代表ascii码值为i的字符不再考虑
+        int[] map = new int[26];
+        for (int i = 0; i < str.length; i++) {
+            map[str[i] - 'a']++;
+        }
+        char[] res = new char[26];
+        int index = 0;
+        int L = 0;
+        int R = 0;
+        while (R != str.length) {
+            // 如果当前字符是不再考虑的，直接跳过
+            // 如果当前字符的出现次数减1之后，后面还能出现，直接跳过
+            if (map[str[R] - 'a'] == -1 || --map[str[R] - 'a'] > 0) {
+                R++;
+            } else { // 当前字符需要考虑并且之后不会再出现了
+                // 在str[L..R]上所有需要考虑的字符中，找到ascii码最小字符的位置
+                int pick = -1;
+                for (int i = L; i <= R; i++) {
+                    if (map[str[i] - 'a'] != -1 && (pick == -1 || str[i] < str[pick])) {
+                        pick = i;
+                    }
+                }
+                // 把ascii码最小的字符放到挑选结果中
+                res[index++] = str[pick];
+                // 在上一个的for循环中，str[L..R]范围上每种字符的出现次数都减少了
+                // 需要把str[pick + 1..R]上每种字符的出现次数加回来
+                for (int i = pick + 1; i <= R; i++) {
+
+                    // 只增加以后需要考虑字符的次数
+                    if (map[str[i] - 'a'] != -1) {
+                        map[str[i] - 'a']++;
+                    }
+                }
+                // 选出的ascii码最小的字符，以后不再考虑了
+                map[str[pick] - 'a'] = -1;
+                // 继续在str[pick + 1......]上重复这个过程
+                L = pick + 1;
+                R = L;
+            }
+        }
+        return String.valueOf(res, 0, index);
+    }
 
 }
